0f3fc56066aa93cb696f1bf743ae7c681c05752f
calleshouse93 <149311867+calleshouse93@users.noreply.github.com>
2023-11-05 11:27:38 -0500

    Add files via upload

---
+++ TimeElapse.java
@@ -0,0 +1,17 @@
+
+public class TimeElapse {
+
+	public static void main(String[] args) {
+		MyDate date1 = new MyDate();
+		MyDate date2 = new MyDate(34355555133101L);
+		
+		
+		System.out.println("Date1: " +date1.getMonth() + "/" + date1.getDay() +
+				"/" + date1.getYear());
+		
+		System.out.println("Date2: " + date2.getMonth() + "/" + date2.getDay() +
+				"/" + date2.getYear());
+
+	}
+
+}
